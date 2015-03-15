7c7eb1ca9cdb8ec0be03edbfcc2a8ba6ff9cf174
Azure0rigin <azure@mail.ccserver.info>
2015-03-16 02:09:36 +0900

    add Item icon

--- src/main/java/info/ccserver/homunculus/HomunculusData.java
+++ src/main/java/info/ccserver/homunculus/HomunculusData.java
@@ -1,8 +1,13 @@
 package info.ccserver.homunculus;
 
 import info.ccserver.homunculus.common.item.ItemFlask;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemModelMesher;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -19,10 +24,26 @@ public class HomunculusData {
 
 		itemFlask = new ItemFlask(nameFlask, tab);
 		GameRegistry.registerItem(itemFlask, nameFlask);
+		
+		if (FMLCommonHandler.instance().getSide() == Side.CLIENT) {
+	 
+	        	ModelBakery.addVariantName(itemFlask,
+						HomunculusMod.MODID.toLowerCase() + ":" + "flask"
+				);
+	 
+	        }
 	}
 
 	public static void register() {
+		if (FMLCommonHandler.instance().getSide() == Side.CLIENT) {
+			registerItemJson(itemFlask, 0, "flask");
+		}
+	}
 
+	public static void registerItemJson(Item item, int metadata, String name) {
+		ItemModelMesher mesher = Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
+		ModelResourceLocation resource = new ModelResourceLocation(HomunculusMod.MODID.toLowerCase() + ":" + name, "inventory");
+		mesher.register(item, metadata, resource);
 	}
 
 	public static class TabHomunculus extends CreativeTabs {
